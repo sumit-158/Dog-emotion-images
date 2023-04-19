84e3f149dde177336087765f05a61d6e85b0a50f
Sumit Kashyap <mr.sumitkrr@gmail.com>
2023-04-19 13:41:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ raw_train_ds = tf.keras.utils.image_dataset_from_directory(
     directory = images,
     seed = 32,
     validation_split = 0.2,
-    image_size = (48,48),
+    image_size = (60,60),
     subset="training"
 )
 
@@ -17,7 +17,7 @@ raw_val_ds = tf.keras.utils.image_dataset_from_directory(
     directory = images,
     seed= 32,
     validation_split=0.2,
-    image_size=(48,48),
+    image_size=(60,60),
     subset = "validation"
 )
 optimization_layer = tf.keras.layers.Rescaling(1./255)
@@ -50,4 +50,4 @@ model.compile(
 history = model.fit(train_ds,epochs=20)
 print("model traning done!")
 model.evaluate(val_ds,verbose=10)
-model.save("Dog_breed.h5")
+model.save("Dog_emotions.h5")
